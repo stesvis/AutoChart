49ca0581eedb8a4d439518f986d7030a85828330
Cristian Merli <stesvis@gmail.com>
2017-04-16 14:32:50 -0600

    Removed old migrations and moved SensioGeneratorBundle under prod

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,14 +21,14 @@ class AppKernel extends Kernel
             //custom bundles
             new AppBundle\AppBundle(),
             new UserBundle\UserBundle(),
-            //new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
+            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+//            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
