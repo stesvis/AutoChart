6b2b85384ec2eb05bf309f6f3d2ea5cca608f583
Cristian Merli <stesvis@gmail.com>
2017-04-25 22:09:01 -0600

    Able to access endpoints with token

--- Notes.php
+++ Notes.php
@@ -15,3 +15,4 @@ Use these:
 ---------------------------------------------------------
 
 http://localhost:8000/app_dev.php/oauth/v2/token?client_id=21_57cfrz5um60ww40w8gkcok4s4soowgk4g00wo0w8scows44880&client_secret=2evhnip4178ko8gsk800k4kskckgko4kc40goowsw0o880ckgw&grant_type=client_credentials
+http://localhost:8000/app_dev.php/oauth/v2/token?client_id=21_57cfrz5um60ww40w8gkcok4s4soowgk4g00wo0w8scows44880&client_secret=2evhnip4178ko8gsk800k4kskckgko4kc40goowsw0o880ckgw&grant_type=password&username=admintest&password=password
--- src/AppApiBundle/Controller/VehicleController.php
+++ src/AppApiBundle/Controller/VehicleController.php
@@ -2,79 +2,73 @@
 
 namespace AppApiBundle\Controller;
 
-use AppBundle\Entity\Vehicle;
-use AppBundle\Form\VehicleFormType;
-use AppBundle\Includes\StatusEnums;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\Controller\Annotations\Route;
+use FOS\RestBundle\Controller\FOSRestController;
 
-class VehicleController extends Controller
+class VehicleController extends FOSRestController
 {
-    /**
-     * @Route("/api/vehicles", name="api_vehicle_new")
-     * @Method("POST")
-     * @param $request Request
-     * @return Response
-     */
-    public function newAction(Request $request)
-    {
-        $this->denyAccessUnlessGranted('ROLE_USER');
-
-        $data = json_decode($request->getContent(), true);
-
-        $vehicle = new Vehicle();
-        $form = $this->createForm(VehicleFormType::class, $vehicle);
-        $form->submit($data);
-
-        $vehicle->setCreatedAt(new \DateTime('now'));
-        $vehicle->setModifiedAt(new \DateTime('now'));
-        $vehicle->setCreatedBy($this->getUser());
-        $vehicle->setModifiedBy($this->getUser());
-        $vehicle->setStatus(StatusEnums::Active);
-        $vehicle->setName($vehicle->getYear() . ' ' . $vehicle->getMake() . ' ' . $vehicle->getModel());
-
-
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($vehicle);
-        $em->flush();
-
-        $response = new Response('Vehicle created', 201);
-        $response->headers->set('Location', 'vehicles/' . $vehicle->getId());
-        return $response;
-    }
+//    /**
+//     * @Route("/api/vehicles", name="api_vehicle_new")
+//     * @Method("POST")
+//     * @param $request Request
+//     * @return Response
+//     */
+//    public function newAction(Request $request)
+//    {
+//        $this->denyAccessUnlessGranted('ROLE_USER');
+//
+//        $data = json_decode($request->getContent(), true);
+//
+//        $vehicle = new Vehicle();
+//        $form = $this->createForm(VehicleFormType::class, $vehicle);
+//        $form->submit($data);
+//
+//        $vehicle->setCreatedAt(new \DateTime('now'));
+//        $vehicle->setModifiedAt(new \DateTime('now'));
+//        $vehicle->setCreatedBy($this->getUser());
+//        $vehicle->setModifiedBy($this->getUser());
+//        $vehicle->setStatus(StatusEnums::Active);
+//        $vehicle->setName($vehicle->getYear() . ' ' . $vehicle->getMake() . ' ' . $vehicle->getModel());
+//
+//
+//        $em = $this->getDoctrine()->getManager();
+//        $em->persist($vehicle);
+//        $em->flush();
+//
+//        $response = new Response('Vehicle created', 201);
+//        $response->headers->set('Location', 'vehicles/' . $vehicle->getId());
+//        return $response;
+//    }
+//
+//    /**
+//     * @Route("/api/vehicles")
+//     * @Method("GET")
+//     * @return Response
+//     */
+//    public function indexAction()
+//    {
+//        $clientManager = $this->container->get('fos_oauth_server.client_manager.default');
+//        $client = $clientManager->createClient();
+//        $client->setRedirectUris(array('http://stesvis.com'));
+//        $client->setAllowedGrantTypes(array('token', 'authorization_code'));
+//        $clientManager->updateClient($client);
+//
+//        return $this->redirect($this->generateUrl('fos_user_security_login', array(
+//            'client_id' => $client->getPublicId(),
+//            'redirect_uri' => 'http://stesvis.com',
+//            'response_type' => 'code'
+//        )));
+//
+//        $this->denyAccessUnlessGranted('ROLE_USER');
+//
+//        return new Response('indexAction');
+//    }
 
     /**
      * @Route("/api/vehicles")
-     * @Method("GET")
-     * @return Response
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction()
-    {
-        $clientManager = $this->container->get('fos_oauth_server.client_manager.default');
-        $client = $clientManager->createClient();
-        $client->setRedirectUris(array('http://stesvis.com'));
-        $client->setAllowedGrantTypes(array('token', 'authorization_code'));
-        $clientManager->updateClient($client);
-
-        return $this->redirect($this->generateUrl('fos_user_security_login', array(
-            'client_id' => $client->getPublicId(),
-            'redirect_uri' => 'http://stesvis.com',
-            'response_type' => 'code'
-        )));
-
-        $this->denyAccessUnlessGranted('ROLE_USER');
-
-        return new Response('indexAction');
-    }
-
-    /**
-     * @Route("/api/demo")
-     * @return mixed
-     */
-    public function getDemosAction()
+    public function getVehiclesAction()
     {
         $data = array("hello" => "world");
         $view = $this->view($data);
